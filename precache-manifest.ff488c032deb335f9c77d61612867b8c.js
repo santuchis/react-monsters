bfcc548d41075589af48fb9ed2e81d41c69bd623
santiago.milanese <santiago.milanese@gmail.com>
2020-04-20 17:04:10 -0300

    Updates

---
+++ precache-manifest.ff488c032deb335f9c77d61612867b8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b9716a825d14022c8836d7fe7b35cd",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "aec4eec59f83cd566ea1",
+    "url": "/react-monsters/static/css/main.3e336979.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec4eec59f83cd566ea1",
+    "url": "/react-monsters/static/js/main.c61cf489.chunk.js"
+  },
+  {
+    "revision": "10a25d6082fe69f22468",
+    "url": "/react-monsters/static/js/runtime-main.578e257e.js"
+  }
+]);
